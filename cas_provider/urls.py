75243382e5e884e695597d76d3a8e849eea0a676
Fred Wenzel <fwenzel@mozilla.com>
2010-04-14 19:10:30 +0200

    allow URLs without trailing slashes: not all CAS clients follow redirects.

--- cas_provider/urls.py
+++ cas_provider/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls.defaults import patterns, url
 
 
 urlpatterns = patterns('cas_provider.views',
-    url(r'^login/$', 'login', name='cas_login'),
-    url(r'^validate/$', 'validate', name='cas_validate'),
-    url(r'^serviceValidate/$', 'service_validate',
+    url(r'^login/?$', 'login', name='cas_login'),
+    url(r'^validate/?$', 'validate', name='cas_validate'),
+    url(r'^serviceValidate/?$', 'service_validate',
         name='cas_service_validate'),
-    url(r'^logout/$', 'logout', name='cas_logout'),
+    url(r'^logout/?$', 'logout', name='cas_logout'),
 )
