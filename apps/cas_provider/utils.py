bdac304e710902658d5bf92e999e6b64b972493f
Fred Wenzel <fwenzel@mozilla.com>
2010-08-04 17:16:15 +0200

    all right, floating point arithmetic is evil.

--- apps/cas_provider/utils.py
+++ apps/cas_provider/utils.py
@@ -1,27 +1,32 @@
-import math
 import os
-import string
 
 from cas_provider.models import ServiceTicket, LoginTicket
 
+
 def _generate_string(length=8):
-    """Generates a random string of the requested length. Used for creation of tickets."""
+    """
+    Generates a random string of the requested length. Used for creation of
+    tickets.
+    """
     assert length >= 0
-    randlen = int(math.ceil(length/2.0))
+
+    # Just enough randomness for our string length, hex-encoded.
+    randlen = (length >> 1) + (length % 2)
     return os.urandom(randlen).encode('hex')[:length]
 
+
 def create_service_ticket(user, service):
-    """
-    Creates a new service ticket for the specified user and service.
-   Uses _generate_string.
-    """
-    ticket_string = 'ST-' + _generate_string(29) # Total ticket length = 29 + 3 = 32
+    """Creates a new service ticket for the specified user and service."""
+
+    # Total ticket length = 29 + 3 = 32
+    ticket_string = 'ST-' + _generate_string(29)
     ticket = ServiceTicket(service=service, user=user, ticket=ticket_string)
     ticket.save()
     return ticket
 
+
 def create_login_ticket():
-    """Creates a new login ticket for the login form. Uses _generate_string."""
+    """Creates a new login ticket for the login form."""
     ticket_string = 'LT-' + _generate_string(29)
     ticket = LoginTicket(ticket=ticket_string)
     ticket.save()
